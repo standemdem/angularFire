cfaeeb5430727c43a863e7f84e3935cef46b5836
ghjuvan <vaevconsult@gmail.com>
2018-08-17 15:57:21 +0200

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCr6Sy0wuJnyMzP3lPDCSfBdwjAJdFZ8jI',
+    authDomain: 'white-rabbit-manager.firebaseapp.com',
+    databaseURL: 'https://white-rabbit-manager.firebaseio.com',
+    projectId: 'white-rabbit-manager',
+    storageBucket: 'white-rabbit-manager.appspot.com',
+    messagingSenderId: '609303050921'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCr6Sy0wuJnyMzP3lPDCSfBdwjAJdFZ8jI',
+    authDomain: 'white-rabbit-manager.firebaseapp.com',
+    databaseURL: 'https://white-rabbit-manager.firebaseio.com',
+    projectId: 'white-rabbit-manager',
+    storageBucket: 'white-rabbit-manager.appspot.com',
+    messagingSenderId: '609303050921'
+  }
 };
 
 /*
